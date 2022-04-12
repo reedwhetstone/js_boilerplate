a297d2a7064df27875a25a9bf346c9b4f760beb8
Reed Whetstone <rwhetstone0934@gmail.com>
2022-04-12 12:10:21 -0600

    altered initial to pure css & added font-awesome

--- boilerplate.js
+++ boilerplate.js
@@ -1,8 +1,8 @@
 /** @format */
 
-const { exec, execSync } = require("child_process");
-const fs = require("fs");
-const folderName = process.argv[2] || "New_Proj";
+const { exec, execSync } = require('child_process');
+const fs = require('fs');
+const folderName = process.argv[2] || 'newProject';
 
 try {
   // set file location for creation. this ensures the file is created on desktop
@@ -23,26 +23,45 @@ try {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" /> -->  
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="container mx-auto my-2">
-        <h1 class="text-center">HELLO WORLD</h1>
-    </div>
+    <h1>Hello World</h1>
 
     <script src="app.js"></script>
 </body>
 
 </html>`
   );
-  fs.writeFileSync(`${folderName}/app.js`, "");
-  fs.writeFileSync(`${folderName}/style.css`, "");
+  fs.writeFileSync(`${folderName}/app.js`, '');
+  fs.writeFileSync(
+    `${folderName}/style.css`,
+    `@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+}`
+  );
   // executes vscode to open the new folder and contents. must have vscode set up to work with terminal.
   execSync(`code ${folderName}`);
 } catch (e) {
-  console.log("SOMETHING WENT WRONG!");
+  console.log('SOMETHING WENT WRONG!');
   console.log(e);
 }
